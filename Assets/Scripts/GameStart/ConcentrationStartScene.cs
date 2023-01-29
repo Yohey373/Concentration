cfecb2e7a8c40054e6083830b9c16610bb8e5199
Yohey373 <load5123874@gmail.com>
2023-01-29 10:38:05 +0900

    Add 2P munu

--- Assets/Scripts/GameStart/ConcentrationStartScene.cs
+++ Assets/Scripts/GameStart/ConcentrationStartScene.cs
@@ -2,18 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class ConcentrationStartScene : MonoBehaviour
 {
     [SerializeField]
-    Button startButton;
+    private Button OnePlayerStartButton;
+    [SerializeField]
+    private Button TwoPlayerStartButton;
 
     void Start()
     {
-        startButton.onClick.AddListener(() =>
+        OnePlayerStartButton.onClick.AddListener(() =>
+        {
+            GameSceneUtil.Instance.SingleSceneTransration(
+                ConcentrationGameStringResource.CNCENTRATION_GAME_MAIN_SCENE,
+                () => OnePlayerStartButtonAction());
+        });
+
+        TwoPlayerStartButton.onClick.AddListener(() =>
         {
             GameSceneUtil.Instance.SingleSceneTransration(
                 ConcentrationGameStringResource.CNCENTRATION_GAME_MAIN_SCENE);
         });
+
     }
+
+    public void OnePlayerStartButtonAction()
+    {
+        // GameMain‚É‚¢‚éConcentrationGameProgresionManager‚ðŽæ“¾
+        var gameProgressionManagerGameObject = 
+            GameSceneUtil.Instance.NextSceneRootGetGameObjects.Where(x => x.GetComponent<ConcentrationGameProgressionManager>()).FirstOrDefault();
+
+        if (gameProgressionManagerGameObject != null)
+        {
+            var gameProgressionManager =
+                gameProgressionManagerGameObject.GetComponent<ConcentrationGameProgressionManager>();
+            gameProgressionManager.GameMode = ConcentrationGameProgressionManager.GameModes.CPUCardIsComputersChoice;
+        }
+    }
+
 }
